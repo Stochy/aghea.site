363921174027318ed37175c5bc329614bf0a4a3a
agcrisbp <agcrisbp@proton.me>
2024-12-09 19:16:27 +0700

    add Lanyard Spotify activity

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Clock } from "./Clock";
-export { default as Discord } from "./Discord";
-export { default as YoutubeMusic } from "./YoutubeMusic";
-export { default as GenericMeta } from "./GenericMeta";
-export { default as Spo } from "./Spo";
-export { default as Spotify } from "./Spotify";
