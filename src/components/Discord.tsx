a540e066b3dd7bca77e7741bf73e612aaeca4e4d
agcrisbp <agcrisbp@email.com>
2025-01-07 22:16:53 +0700

    Fix Avatar Layout

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
+import Image from "next/future/image";
 import Link from "next/link";
 import { Icon } from '@iconify/react';
 import formatDistanceStrict from "date-fns/formatDistanceStrict";
